23121e419a839207a58a64a63caeed21ecee5f71
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-08-23 12:38:10 +0600

    update

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { UserProfileRoute } from "../modules/users/userProfile/userProfile.route
 import { FormRoute } from "../modules/test_form/formCreate/form.route";
 import { BankRoute } from "../modules/bank/bank.route";
 import { FormResponseRoute } from "../modules/test_form/formResponse/formResponse.route";
+import { FinanceRoute } from "../modules/income_expanse/finance/finance.route";
 
 const router = Router();
 const apiRoutes = [
@@ -14,6 +15,7 @@ const apiRoutes = [
   { path: "/form", route: FormRoute },
   { path: "/form", route: FormResponseRoute },
   { path: "/bank", route: BankRoute },
+  { path: "/finance", route: FinanceRoute },
 ];
 apiRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
